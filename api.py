6ec2a38608171d422403dc8f4891459897dc2c2c
GuessWu <138514697+GuessWu@users.noreply.github.com>
2024-04-07 22:38:53 +0200

    Update api.py

--- api.py
+++ api.py
@@ -1,46 +1,45 @@
-import os
-import joblib
+from flask import Flask, request, jsonify
 import pandas as pd
-from flask import Flask, request
-from flasgger import Swagger
+import nltk
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
+import re
+from nltk.corpus import stopwords
+import os
+from nltk.stem import WordNetLemmatizer
+import joblib
 
 app = Flask(__name__)
-Swagger(app)
 
-pipeline_filename = os.path.join('trained_model', 'lr_predictions.pkl')
-pipeline = joblib.load(pipeline_filename)
+# Wczytanie wytrenowanego modelu i CountVectorizer
+model = joblib.load("lr_predictions.pkl")
+cv = joblib.load("count_vectorizer.pkl")
 
+# Funkcja do przetwarzania tekstu
+def preprocess_text(text):
+    lemmatizer = WordNetLemmatizer()
+    text = re.sub('[^a-zA-Z]', ' ', text)
+    text = text.lower()
+    text = text.split()
+    text = [word for word in text if word not in stopwords.words('english')]
+    text = ' '.join(text)
+    return text
+
+# Funkcja do przewidywania etykiety dla tekstu
+def predict_label(text):
+    preprocessed_text = preprocess_text(text)
+    text_vector = cv.transform([preprocessed_text])
+    prediction = model.predict(text_vector)
+    return prediction[0]
+
+# Endpoint dla przetwarzania danych
 @app.route('/predict', methods=['POST'])
 def predict():
-    """
-    Predict political bias for new data
-    ---
-    parameters:
-      - name: body
-        in: body
-        required: true
-        schema:
-          id: PredictInput
-          properties:
-            text:
-              type: string
-              description: Input article text
-    responses:
-      200:
-        description: Successfully predicted political bias
-        schema:
-          id: PredictOutput
-          properties:
-            prediction:
-              type: string
-              description: Predicted political bias (left/right)
-    """
     data = request.get_json()
-
-    new_data = pd.DataFrame(data, index=[0])
-
-    prediction = pipeline.predict(new_data)
-    return f"Model prediction is {prediction}"
+    input_text = data['text']
+    prediction = predict_label(input_text)
+    return jsonify({'prediction': prediction})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	port = int(os.environ.get("PORT", 5000))
+	app.run(debug=True, host='::', port=port)
