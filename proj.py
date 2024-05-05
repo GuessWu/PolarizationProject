ec5055e353248c722779f4821bfe8a960128f677
GuessWu <138514697+GuessWu@users.noreply.github.com>
2024-05-05 13:00:40 +0200

    adding tdidf algorithm

--- proj.py
+++ proj.py
@@ -1,10 +1,11 @@
 # reading data
 
-import pandas as pd
-import nltk
-from sklearn import metrics
+import pandas as pd # type: ignore
+import nltk# type: ignore
+from sklearn import metrics# type: ignore
 import os
-import joblib
+import joblib # type: ignore
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 datasur = pd.read_csv(r'C:\\Users\\gessn\\OneDrive\\Pulpit\\Nauka\\python\\projekt\\labeledtext.csv')
 dataright = datasur[datasur['label']=='right']
@@ -16,7 +17,7 @@ data.columns = ['title', 'text', 'label']
 data=data.dropna()
 
 
-import nltk
+import nltk # type: ignore
 
 text = list(data['text'])
 
@@ -24,9 +25,9 @@ text = list(data['text'])
 
 import re
 
-from nltk.corpus import stopwords
+from nltk.corpus import stopwords # type: ignore
 
-from nltk.stem import WordNetLemmatizer
+from nltk.stem import WordNetLemmatizer # type: ignore
 
 lemmatizer = WordNetLemmatizer()
 
@@ -59,7 +60,7 @@ y = data['label']
 
 # rozdział datasetu
 
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split # type: ignore
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=123)
 
@@ -72,7 +73,7 @@ print('Testing Data : ', X_test.shape)
 
 # BOW
 
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer # type: ignore
 
 cv = CountVectorizer()
 
@@ -81,24 +82,29 @@ print (X_train_cv)
 
 X_train_cv.shape
 
+#TD-IDF
+
+tfidf=TfidfVectorizer()
+x_train_td=tfidf.fit_transform(X_train)
+
 # trenowanie
 
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegression # type: ignore
 # jakie argumenty i wartości do nich przypisane będą odpowiednie?
 lr = LogisticRegression(solver='liblinear', random_state=0)
 
-lr=lr.fit(X_train_cv, y_train)
+lr=lr.fit(x_train_td, y_train)
 print (lr)
 
 # transform X_test using CV
 
 X_test_cv = cv.transform(X_test)
-
+X_test_td=tfidf.transform(X_test)
 
 # predykcje
 
-predictions = lr.predict(X_test_cv)
-predictionsy = lr.predict_proba(X_test_cv)
+predictions = lr.predict(X_test_td)
+predictionsy = lr.predict_proba(X_test_td)
 print(predictions)
 print (predictionsy)
 #jak zwrócić wartości liczbowe przewidywań?
@@ -110,16 +116,17 @@ print (df)
 
 
 #obliczyć accuracy
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score # type: ignore
 score=accuracy_score(y_test,predictions)
 print (score)
 
 # Create the 'trained_model' folder if it doesn't exist
 if not os.path.exists('trained_model'):
-        os.makedirs('trained_model')
+       os.makedirs('trained_model')
 
 # Save the trained pipeline to the 'trained_model' folder
 pipeline_filename = os.path.join('trained_model', 'lr_predictions.pkl')
 joblib.dump(lr, pipeline_filename)
-joblib.dump(cv, 'count_vectorizer.pkl')
+pipeline_filename2 = os.path.join('trained_model', 'count2.pkl')
+joblib.dump(cv, pipeline_filename2)
 print(f"Trained pipeline saved as {pipeline_filename}")
