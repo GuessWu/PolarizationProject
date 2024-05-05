252d67d8e2892261e114230923b9d0e99ae51589
GuessWu <138514697+GuessWu@users.noreply.github.com>
2024-05-05 13:52:26 +0200

    adding tfidf module and changing return method

--- api.py
+++ api.py
@@ -1,8 +1,6 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify,render_template
 import pandas as pd
 import nltk
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.linear_model import LogisticRegression
 import re
 from nltk.corpus import stopwords
 import os
@@ -13,7 +11,13 @@ app = Flask(__name__)
 
 # Wczytanie wytrenowanego modelu i CountVectorizer
 model = joblib.load("lr_predictions.pkl")
-cv = joblib.load("count_vectorizer.pkl")
+cv = joblib.load("count.pkl")
+tfidf=joblib.load("tfidf.pkl")
+nltk.download('stopwords')
+
+@app.route('/')
+def home():
+    return render_template('text_article.html')
 
 # Funkcja do przetwarzania tekstu
 def preprocess_text(text):
@@ -28,18 +32,28 @@ def preprocess_text(text):
 # Funkcja do przewidywania etykiety dla tekstu
 def predict_label(text):
     preprocessed_text = preprocess_text(text)
-    text_vector = cv.transform([preprocessed_text])
-    prediction = model.predict(text_vector)
+    text_vector = tfidf.transform([preprocessed_text])
+    prediction = model.predict_proba(text_vector)
     return prediction[0]
 
 # Endpoint dla przetwarzania danych
-@app.route('/predict', methods=['GET','POST'])
+@app.route('/predict', methods=['POST', 'GET'])
 def predict():
-    data = request.get_json()
-    input_text = data['text']
-    prediction = predict_label(input_text)
-    return jsonify({'prediction': prediction})
+    if request.method == 'POST':
+        input_text = request.form['text']
+        prediction = predict_label(input_text)
+        return render_template('return_bias.html', prediction=prediction)
+    elif request.method == 'GET':
+        return render_template('text_article.html', prediction=None)
+
+@app.route('/health', methods=['GET'])
+def health():    
+	return jsonify({
+    	'response' : True,
+	})
+
 
 if __name__ == '__main__':
 	port = int(os.environ.get("PORT", 5000))
 	app.run(debug=True, host='::', port=port)
+
